2d810c02d789ea3f00c717b135b710369793bfce
ntd1190 <31530791+ntd1190@users.noreply.github.com>
2017-10-27 16:52:30 +0700

    2710f

--- SongAn.QLTS/01 Master/05 Presentations/QLTS/QLTSMAIN/Scripts/angular/ChotSoLieuThang/controller/ChotSoLieuThangListCtrl.js
+++ SongAn.QLTS/01 Master/05 Presentations/QLTS/QLTSMAIN/Scripts/angular/ChotSoLieuThang/controller/ChotSoLieuThangListCtrl.js
@@ -1,1 +1,137 @@
-﻿
+﻿(function () {
+    'use strict';
+    angular.module('app').controller('ChotSoLieuThangListCtrl', function ($rootScope, $scope, utility, ChotSoLieuThangService) {
+        /* PRIVATE */
+
+        var vm = this;
+        //HOT-KEY       
+        vm.keys = {
+
+            //press F8 -> search
+            F8: function (name, code) {
+            }
+        };
+        //end HOT-KEY
+        /* VIEW MODEL */
+        vm.controllerId = 'ChotSoLieuThangListCtrl';
+
+        vm.data = {};
+        vm.data.listQuyenTacVu = [];
+        vm.data.userInfo = {};
+
+        vm.onInitView = function (config) {
+            if (config && config.userInfo) {
+                vm.data.listQuyenTacVu = config.userInfo.DsQuyenTacVu.split(',');
+                vm.data.userInfo = config.userInfo;
+                //setEnableButton();
+            }
+            initEventListener();
+        }
+
+        activate();
+
+        function activate() {
+            var d = new Date();
+            vm.data.nam = d.getFullYear().toString();
+            vm.data.thang = '01';//moment().format('MM');
+        }
+
+        /* ACTION FUNCTION */
+        vm.action = {};
+
+        vm.action.ChotThang = function () {
+            var thangNam = vm.data.thang + vm.data.nam.substr(vm.data.nam.length - 2);
+            alert(thangNam);
+
+            return;
+            utility.addloadding($('body'));
+            var data = {};
+            data.ThangNam = vm.data.thangNam;
+            data.CoSoId = vm.data.userInfo.CoSoId || 0;
+            data.LoginId = vm.data.userInfo.NhanVienId || 0;
+
+            ChotSoLieuThangService.insert(data)
+                .then(function success(result) {
+                    utility.removeloadding();
+                    if (parseInt(result.data.data[0]["ID"]) < 0) {
+                        if (parseInt(result.data.data[0]["ID"]) == -1)
+                            utility.AlertError("Tháng " + vm.data.thang + " năm " + vm.data.nam + " đã chốt !");
+                        else if (parseInt(result.data.data[0]["ID"]) == -2 || parseInt(result.data.data[0]["ID"]) == -3)
+                            utility.AlertError("Tháng không hợp lệ!");
+                    }
+                    else
+                        utility.AlertSuccess("Chốt số liệu tháng thành công!");
+
+                }, function error(result) {
+                    console.log(result);
+                    utility.removeloadding();
+                    if (result.status === 400) {
+                        alert(result.data.error.message);
+                    } else {
+                        utility.AlertError('Không thể chốt!');
+                    }
+                });
+        };
+
+
+        /* BROADCAST / EMIT / ON FUNCTION */
+        function initEventListener() {
+
+        }
+
+        /* BIZ FUNCTION */
+        function setInitValue() {
+
+        }
+
+        function joinStr(array, property) {
+            var result = '';
+
+            var list = new Array();
+            if (array) {
+                for (var i = 0; i < array.length; i++) {
+                    list.push(array[i][property]);
+                }
+
+                result = list.join(',');
+            } else result = result || '';
+
+            return result;
+        }
+
+    }).directive("keyboard", keyboard);        //HOT-KEY;
+
+    //HOT-KEY
+    function keyboard($document, keyCodes) {
+        return {
+            link: function (scope, element, attrs) {
+
+                var keysToHandle = scope.$eval(attrs.keyboard);
+                var keyHandlers = {};
+
+                // Registers key handlers
+                angular.forEach(keysToHandle, function (callback, keyName) {
+                    var keyCode = keyCodes[keyName];
+                    keyHandlers[keyCode] = { callback: callback, name: keyName };
+                });
+
+                // Bind to document keydown event
+                $document.on("keydown", function (event) {
+
+                    var keyDown = keyHandlers[event.keyCode];
+
+                    // Handler is registered
+                    if (keyDown) {
+                        event.preventDefault();
+
+                        //// Invoke the handler and digest
+                        //scope.$apply(function () {
+                        //    keyDown.callback(keyDown.name, event.keyCode);
+                        //})
+                    }
+                });
+            }
+        }
+    };
+    //end HOT-KEY
+})();
--- SongAn.QLTS/01 Master/05 Presentations/QLTS/QLTSMAIN/Scripts/angular/ChotSoLieuThang/service/ChotSoLieuThangService.js
+++ SongAn.QLTS/01 Master/05 Presentations/QLTS/QLTSMAIN/Scripts/angular/ChotSoLieuThang/service/ChotSoLieuThangService.js
@@ -1,1 +1,39 @@
-﻿
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('ChotSoLieuThangService', ChotSoLieuThangService);
+
+    function ChotSoLieuThangService($http, API_BASE) {
+        var api = {
+            url: API_BASE + 'Api.QLTS/ChotSoLieuThang/',
+            insert: 'ChotSoLieuThang'
+        }
+
+        var service = {
+            insert: insert
+        };
+
+        return service;
+
+        function insert(obj) {
+            if (!obj) {
+                return null;
+            };
+
+            var url = api.url + api.insert;
+            var req = {
+                url: url,
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+                data: $.param({
+                    ThangNam: obj.ThangNam,
+                    CoSoId: obj.CoSoId,
+                    LoginId: obj.LoginId
+                })
+            }
+            return $http(req);
+        }
+    }
+})();
