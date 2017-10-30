5806c22500acd96a5b00ab0b48c266f423c0d901
ntd1190 <31530791+ntd1190@users.noreply.github.com>
2017-10-30 17:02:20 +0700

    3010d

---
+++ SongAn.QLTS/01 Master/05 Presentations/QLTS/QLTSMAIN/Controllers/KhoPhieuXuatKhacController.cs
@@ -0,0 +1,46 @@
+﻿using SongAn.QLTS.Util.Common.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace SongAn.QLTS.UI.QLTSMAIN.Controllers
+{
+    public class KhoPhieuXuatKhacController : BaseController
+    {
+        private string FunctionCode = "CN0050";
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+        [CustomAuthorize(FunctionCodes = "CN0050")]
+        public ActionResult List()
+        {
+            ViewBag.userInfo = LoadUserInfo(FunctionCode);
+            return View();
+        }
+        [CustomAuthorize(FunctionCodes = "CN0050")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("List");
+            }
+            ViewBag.userInfo = LoadUserInfo(FunctionCode);
+            ViewBag.id = id;
+            ViewBag.isEdit = 1;
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult showView(string viewName, string type)
+        {
+            type = string.IsNullOrEmpty(type) ? "Html" : type;
+            ViewData[type] = true;
+            return PartialView(viewName);
+        }
+
+    }
+}
