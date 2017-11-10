cda72b55e7e0a1dc9283aea8cc24aa2e49d7b54f
ntd1190 <31530791+ntd1190@users.noreply.github.com>
2017-11-10 16:32:14 +0700

    1011

---
+++ SongAn.QLKD/01 Master/02 DataAccess Layer/Data.QLKD/NhanVien/GetListNhanVienByProjectionDac.cs
@@ -0,0 +1,108 @@
+﻿using Dapper;
+using Dapper.FastCrud;
+using SongAn.QLKD.Util.Common.Dto;
+using SongAn.QLKD.Util.Common.Repository;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+
+namespace SongAn.QLKD.Data.QLKD.NhanVien
+{
+    public class GetListNhanVienByProjectionDac : BaseRepositoryAsync
+    {
+        #region public properties
+        /// <summary>
+        /// tìm kiếm quick search
+        /// </summary>
+        public string SearchString { get; set; }
+
+        /// <summary>
+        /// Danh sách issue khách hàng
+        /// </summary>
+        ///  
+        public string NhanVienId { get; set; }
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Mệnh đề order by
+        /// </summary>
+        /// <summary>
+        /// Số dòng skip (để phân trang)
+        /// </summary>
+        public int? Skip { get; set; }
+
+        /// <summary>
+        /// Số dòng take (để phân trang)
+        /// </summary>
+        public int? Take { get; set; }
+
+        #endregion
+
+        #region private variable
+
+        ContextDto _context;
+
+        #endregion
+
+        #region constructor
+        /// <summary>
+        /// Ham khoi tao, chi nhan vao bien moi truong va goi lop base
+        /// </summary>
+        /// <param name="context"></param>
+        public GetListNhanVienByProjectionDac(ContextDto context) : base(context.dbQLKDConnection)
+        {
+            OrmConfiguration.DefaultDialect = SqlDialect.MsSql;
+
+            _context = context;
+        }
+        #endregion
+
+        #region init & validate
+        /// <summary>
+        /// Ham khoi tao gia tri mac dinh cho cac bien
+        /// </summary>
+        private void Init()
+        {
+            Skip = Skip != null ? Skip.Value : 0;
+
+            Take = Take != null ? Take.Value : 100;
+        }
+
+        /// <summary>
+        /// Ham chuan hoa gia tri cac bien
+        /// </summary>
+        private void Validate()
+        {
+
+        }
+
+        #endregion
+
+        #region execute
+
+        /// <summary>
+        /// Ham xu ly chinh, chi nhan 1 bien moi truong
+        /// </summary>
+        /// <param name="context">Bien moi truong</param>
+        /// <returns></returns>
+        public virtual async Task<IEnumerable<dynamic>> Execute()
+        {
+            Init();
+            Validate();
+
+            return await WithConnection(async c =>
+            {
+                var p = new DynamicParameters(this);
+                var objResult = await c.QueryAsync<dynamic>(
+                    sql: "sp_KD_NhanVien_GetListNhanVienByCriteria",
+                    param: p,
+                    commandType: CommandType.StoredProcedure);
+
+                return objResult;
+            });
+        }
+
+        #endregion
+    }
+}
