29496706fd7eb30459c487c99bc233b2f81ece38
ntd1190 <31530791+ntd1190@users.noreply.github.com>
2017-11-10 16:31:53 +0700

    1011

---
+++ SongAn.QLKD/01 Master/01 Entity Layer/Entity.QLKD/Entity/KDNhanVienChiTiet.cs
@@ -0,0 +1,31 @@
+﻿/*****************************************************************************
+1. Create Date : 2017.08.05
+2. Creator     : Nguyen Ngoc Tan
+3. Description : NguoiDung entity
+4. History     : 2017.08.05(Nguyen Ngoc Tan) - Tao moi
+*****************************************************************************/
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace SongAn.QLKD.Entity.QLKD.Entity
+{
+    /// <summary>
+    /// A class which represents the NguoiDung table.
+    /// </summary>
+	[Table("KDNhanVienChiTiet")]
+    public partial class KDNhanVienChiTiet
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int NhanVienId { get; set; }
+        public virtual int NhomKinhDoanhId { get; set; }
+        public virtual string CachLamViec { get; set; }
+        public virtual string TinhCach { get; set; }
+        public virtual string SoThich { get; set; }
+        public virtual string ThoiQuen { get; set; }
+        public virtual string GhiChu { get; set; }
+
+    }
+}
---
+++ SongAn.QLKD/01 Master/01 Entity Layer/Entity.QLKD/Entity/KDNhomKinhDoanh.cs
@@ -0,0 +1,32 @@
+﻿/*****************************************************************************
+1. Create Date : 2017.08.05
+2. Creator     : Nguyen Ngoc Tan
+3. Description : NguoiDung entity
+4. History     : 2017.08.05(Nguyen Ngoc Tan) - Tao moi
+*****************************************************************************/
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace SongAn.QLKD.Entity.QLKD.Entity
+{
+    /// <summary>
+    /// A class which represents the NguoiDung table.
+    /// </summary>
+	[Table("KDNhomKinhDoanh")]
+    public partial class KDNhomKinhDoanh
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int NhomKinhDoanhId { get; set; }
+        public virtual string NhomKinhDoanhMaNhomKinhDoanhId { get; set; }
+        public virtual string TenNhomKinhDoanh { get; set; }
+        public virtual string QuanLy { get; set; }
+        public virtual string SoluongNhanVien { get; set; }
+        public virtual int NguoiTao { get; set; }
+        public virtual string NgayTao { get; set; }
+        public virtual string CtrVersion { get; set; }
+
+    }
+}
