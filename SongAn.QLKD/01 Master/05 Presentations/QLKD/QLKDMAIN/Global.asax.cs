8cb32faa90f372f50526861e8026361bf12da11f
ntd1190 <31530791+ntd1190@users.noreply.github.com>
2017-11-10 16:34:05 +0700

    1011

--- SongAn.QLKD/01 Master/05 Presentations/QLKD/QLKDMAIN/Global.asax.cs
+++ SongAn.QLKD/01 Master/05 Presentations/QLKD/QLKDMAIN/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using Newtonsoft.Json;
+using SongAn.QLKD.UI.QLKDMAIN;
 using SongAn.QLKD.Util.Common.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 
@@ -15,6 +17,7 @@ namespace QLKDMAIN
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
 
