cc6ef82309e5cfc1be252f28bcc06478b855d040
ntd1190 <31530791+ntd1190@users.noreply.github.com>
2017-11-27 17:02:10 +0700

    2711

---
+++ SongAn.QLKD/01 Master/01 Entity Layer/Entity.QLKD/Entity/KDChiTieu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace SongAn.QLKD.Entity.QLKD.Entity
+{
+    [Table("KDChiTieu")]
+    public partial class KDChiTieu
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int ChiTieuId { get; set; }
+        public virtual string Nam { get; set; }
+        public virtual int NguoiTao { get; set; }
+        public virtual DateTime NgayTao { get; set; }
+        public virtual int? CtrVersion { get; set; }
+
+    }
+}
---
+++ SongAn.QLKD/01 Master/01 Entity Layer/Entity.QLKD/Entity/KDChiTieuChiTiet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace SongAn.QLKD.Entity.QLKD.Entity
+{
+    [Table("KDChiTieuChiTiet")]
+    public partial class KDChiTieuChiTiet
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int ChiTieuChiTietId { get; set; }
+        public virtual int ChiTieuId { get; set; }
+        public virtual int NhanVienId { get; set; }
+        public virtual decimal Thang1 { get; set; }
+        public virtual decimal Thang2 { get; set; }
+        public virtual decimal Thang3 { get; set; }
+        public virtual decimal Thang4 { get; set; }
+        public virtual decimal Thang5 { get; set; }
+        public virtual decimal Thang6 { get; set; }
+        public virtual decimal Thang7 { get; set; }
+        public virtual decimal Thang8 { get; set; }
+        public virtual decimal Thang9 { get; set; }
+        public virtual decimal Thang10 { get; set; }
+        public virtual decimal Thang11 { get; set; }
+        public virtual decimal Thang12 { get; set; }
+        public virtual DateTime NgayCapNhat { get; set; }
+
+
+    }
+}
