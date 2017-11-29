c522462c81fdca5ec3becbf83e399e02248d3280
ntd1190 <31530791+ntd1190@users.noreply.github.com>
2017-11-29 15:48:03 +0700

    2911

---
+++ SongAn.QLKD/01 Master/02 DataAccess Layer/Data.QLKD/CrystalReport/ReportBaoGiaByIdDac.cs
@@ -0,0 +1,81 @@
+﻿
+using Dapper;
+using Dapper.FastCrud;
+using SongAn.QLKD.Util.Common.Dto;
+using SongAn.QLKD.Util.Common.Repository;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+
+namespace SongAn.QLKD.Data.QLKD.CrystalReport
+{
+    public class ReportBaoGiaByIdDac : BaseRepositoryDataset
+    {
+        #region public properties
+
+        public string BaoGiaId { get; set; }
+
+        #endregion
+
+        #region private variable
+        ContextDto _context;
+        #endregion
+
+        #region constructor
+        /// <summary>
+        /// Ham khoi tao, chi nhan vao bien moi truong va goi lop base
+        /// </summary>
+        /// <param name="context"></param>
+        public ReportBaoGiaByIdDac(ContextDto context) : base(context.dbQLKDConnection)
+        {
+            OrmConfiguration.DefaultDialect = SqlDialect.MsSql;
+
+            _context = context;
+        }
+        #endregion
+
+        #region init & validate
+        /// <summary>
+        /// Ham khoi tao gia tri mac dinh cho cac bien
+        /// </summary>
+        private void Init()
+        {
+
+        }
+
+        /// <summary>
+        /// Ham chuan hoa gia tri cac bien
+        /// </summary>
+        private void Validate()
+        {
+
+        }
+
+        #endregion
+
+        #region execute
+
+        /// <summary>
+        /// Ham xu ly chinh, chi nhan 1 bien moi truong
+        /// </summary>
+        /// <param name="context">Bien moi truong</param>
+        /// <returns></returns>
+        public virtual DataSet ExecuteDac()
+        {
+            Init();
+            Validate();
+
+            List<SqlParameter> prm = new List<SqlParameter>()
+            {
+                 new SqlParameter("@BaoGiaId", SqlDbType.VarChar) {Value = BaoGiaId},
+            };
+            DataSet ds = getData("sp_KD_BaoGia_GetListReportBaoGiaById", prm);
+            return ds;
+
+        }
+
+        #endregion
+    }
+}
---
+++ SongAn.QLKD/01 Master/03 Business Layer/Biz.QLKD/CrystalReport/ReportDonHangByIdBiz.cs
@@ -0,0 +1,75 @@
+﻿
+using SongAn.QLKD.Data.QLKD.CrystalReport;
+using SongAn.QLKD.Util.Common.Dto;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace SongAn.QLKD.Biz.QLKD.CrystalReport
+{
+    public class ReportDonHangByIdBiz : ReportDonHangByIdDac
+    {
+        #region public properties
+
+        #endregion
+
+        #region private variable
+
+        #endregion
+
+        #region constructor
+        /// <summary>
+        /// Ham khoi tao, chi nhan vao bien moi truong va goi lop base
+        /// </summary>
+        /// <param name="context"></param>
+        public ReportDonHangByIdBiz(ContextDto context) : base(context)
+        {
+
+        }
+        #endregion
+
+        #region init & validate
+        /// <summary>
+        /// Ham khoi tao gia tri mac dinh cho cac bien
+        /// </summary>
+        private void Init()
+        {
+
+        }
+
+        /// <summary>
+        /// Ham chuan hoa gia tri cac bien
+        /// </summary>
+        private void Validate()
+        {
+
+        }
+
+        #endregion
+
+        #region execute
+
+        /// <summary>
+        /// Ham xu ly chinh, chi nhan 1 bien moi truong
+        /// </summary>
+        /// <param name="context">Bien moi truong</param>
+        /// <returns></returns>
+        public virtual DataSet ExecuteBiz()
+        {
+            Init();
+            Validate();
+
+            // to do:
+            // biz se thuc hien viec abc o day truoc khi goi dac
+
+            // goi lai ham execute cua tang dac
+            var result = this.ExecuteDac();
+
+            // to do:
+            // biz se thuc hien viec abc voi result truoc khi return
+            return result;
+        }
+
+        #endregion
+    }
+}
