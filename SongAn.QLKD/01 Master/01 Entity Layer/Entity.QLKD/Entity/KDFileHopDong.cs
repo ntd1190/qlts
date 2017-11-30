3ff916bdf09566537df61707f60ca7f110dde2d5
ntd1190 <31530791+ntd1190@users.noreply.github.com>
2017-11-30 16:55:49 +0700

    3011

---
+++ SongAn.QLKD/01 Master/01 Entity Layer/Entity.QLKD/Entity/KDFileHopDong.cs
@@ -0,0 +1,35 @@
+﻿/*****************************************************************************
+1. Create Date : 2017.11.16
+2. Creator     : NGUYỄN THANH BÌNH
+3. Description : 
+4. History     : 2017.11.16 (NGUYỄN THANH BÌNH) - Tao moi
+*****************************************************************************/
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SongAn.QLKD.Entity.QLKD.Entity
+{
+    /// <summary>
+    /// A class which represents the NguoiDung table.
+    /// </summary>
+	[Table("KDFileHopDong")]
+    public partial class KDFileHopDong
+    {
+        public virtual int FileHopDongId { get; set; } 
+        public virtual int HopDongId { get; set; }
+        public virtual string DaLam { get; set; }
+        public virtual string NguoiLam { get; set; }
+        public virtual string DaChuyen { get; set; }
+        public virtual string NguoiChuyen { get; set; }
+        public virtual DateTime? NgayChuyen { get; set; }
+        public virtual string NguoiNhan { get; set; }
+        public virtual DateTime? NgayNhan { get; set; }
+        public virtual string FileMem { get; set; }
+        public virtual string FileCung { get; set; }
+        public virtual string GhiChu { get; set; }
+        public virtual int NguoiTao { get; set; }
+        public virtual DateTime NgayTao { get; set; }
+        public virtual int CtrVersion { get; set; }
+
+    }
+}
---
+++ SongAn.QLKD/01 Master/02 DataAccess Layer/Data.QLKD/CrystalReport/ReportBaoCaoDoanhThuByCriteriaDac.cs
@@ -0,0 +1,102 @@
+﻿
+using Dapper;
+using Dapper.FastCrud;
+using SongAn.QLKD.Util.Common.Dto;
+using SongAn.QLKD.Util.Common.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+
+namespace SongAn.QLKD.Data.QLKD.CrystalReport
+{
+    public class ReportBaoCaoDoanhThuByCriteriaDac : BaseRepositoryDataset
+    {
+        #region public properties
+
+        public string UserId { get; set; }
+
+        public string NhanVienId { get; set; }
+
+        public string Search { get; set; }
+        public string SearchLoaiHopDongId { get; set; }
+        public DateTime TuNgay { get; set; }
+        public DateTime DenNgay { get; set; }
+        public string OrderClause { get; set; }
+
+        public int? Skip { get; set; }
+
+        public int? Take { get; set; }
+
+        #endregion
+
+        #region private variable
+        ContextDto _context;
+        #endregion
+
+        #region constructor
+        /// <summary>
+        /// Ham khoi tao, chi nhan vao bien moi truong va goi lop base
+        /// </summary>
+        /// <param name="context"></param>
+        public ReportBaoCaoDoanhThuByCriteriaDac(ContextDto context) : base(context.dbQLKDConnection)
+        {
+            OrmConfiguration.DefaultDialect = SqlDialect.MsSql;
+
+            _context = context;
+        }
+        #endregion
+
+        #region init & validate
+        /// <summary>
+        /// Ham khoi tao gia tri mac dinh cho cac bien
+        /// </summary>
+        private void Init()
+        {
+
+        }
+
+        /// <summary>
+        /// Ham chuan hoa gia tri cac bien
+        /// </summary>
+        private void Validate()
+        {
+
+        }
+
+        #endregion
+
+        #region execute
+
+        /// <summary>
+        /// Ham xu ly chinh, chi nhan 1 bien moi truong
+        /// </summary>
+        /// <param name="context">Bien moi truong</param>
+        /// <returns></returns>
+        public virtual DataSet ExecuteDac()
+        {
+            Init();
+            Validate();
+
+            List<SqlParameter> prm = new List<SqlParameter>()
+            {
+                 new SqlParameter("@UserId", SqlDbType.VarChar) {Value = UserId},
+                 new SqlParameter("@NhanVienId", SqlDbType.VarChar) {Value = NhanVienId},
+                 new SqlParameter("@Search", SqlDbType.VarChar) {Value = Search},
+                 new SqlParameter("@SearchLoaiHopDongId", SqlDbType.VarChar) {Value = SearchLoaiHopDongId},
+                 new SqlParameter("@TuNgay", SqlDbType.DateTime) {Value = TuNgay},
+                 new SqlParameter("@DenNgay", SqlDbType.DateTime) {Value = DenNgay},
+                 new SqlParameter("@OrderClause", SqlDbType.VarChar) {Value = OrderClause},
+                 new SqlParameter("@Skip", SqlDbType.VarChar) {Value = Skip},
+                 new SqlParameter("@Take", SqlDbType.Int) {Value = 10000}
+            };
+            DataSet ds = getData("sp_KD_BaoCaoDoanhThu_GetListBaoCaoDoanhThuByCriteria", prm);
+            return ds;
+
+        }
+
+        #endregion
+    }
+}
