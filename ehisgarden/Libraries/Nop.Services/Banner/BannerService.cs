58b582d365ccc72ec584e891f9e02c03bacc54d3
ntd1190 <31530791+ntd1190@users.noreply.github.com>
2017-12-11 13:58:16 +0700

    S

---
+++ ehisgarden/Libraries/Nop.Services/Banner/BannerService.cs
@@ -0,0 +1,57 @@
+﻿using Nop.Core.Data;
+using Nop.Core.Domain.Banner;
+using Nop.Services.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Services.Banner
+{
+
+    public partial class BannerService : IBannerService
+    {
+        #region Fields
+
+        private readonly IRepository<BannerPost> _bannerRepository;
+        private readonly IEventPublisher _eventPublisher;
+
+        #endregion
+
+        #region Ctor
+
+        public BannerService(IRepository<BannerPost> bannerRepository,
+            IEventPublisher eventPublisher)
+        {
+            this._bannerRepository = bannerRepository;
+            this._eventPublisher = eventPublisher;
+        }
+
+        public void Delete(BannerPost  banner)
+        {
+            _bannerRepository.Delete(banner);
+        }
+
+        public List<BannerPost> GetAll()
+        {
+            return _bannerRepository.Table.ToList();
+        }
+
+        public BannerPost GetBannerById(int id)
+        {
+            return _bannerRepository.GetById(id);
+        }
+
+        public void Insert(BannerPost banner)
+        {
+            _bannerRepository.Insert(banner);
+        }
+
+        public void Update(BannerPost  banner)
+        {
+            _bannerRepository.Update(banner);
+        }
+        #endregion
+    }
+}
---
+++ ehisgarden/Libraries/Nop.Services/Banner/IBannerService.cs
@@ -0,0 +1,20 @@
+﻿using Nop.Core.Domain.Banner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Services.Banner
+{
+    public partial interface IBannerService
+    {
+        void Insert(BannerPost userMaster);
+
+        void Update(BannerPost userMaster);
+
+        BannerPost GetBannerById(int userId);
+        List<BannerPost> GetAll();
+        void Delete(BannerPost userMaster);
+    }
+}
